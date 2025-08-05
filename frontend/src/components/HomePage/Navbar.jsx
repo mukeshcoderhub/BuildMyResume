10a2995beacd11b45f1c6c6344ea9ee6b5dbfad3
mukeshcoderhub <mukeshcoderhub@gmail.com>
2025-08-05 10:35:21 +0530

    jh

--- frontend/src/components/HomePage/Navbar.jsx
+++ frontend/src/components/HomePage/Navbar.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { GiPaperWindmill } from "react-icons/gi";
 import { IoNewspaperSharp } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
+
 const Navbar = () => {
+    const navigate = useNavigate();
   return (
       <>
           <div className="w-full h-[10vh] flex justify-between items-center">
@@ -9,7 +12,7 @@ const Navbar = () => {
                   <p className='flex items-center text-black text-xl font-semibold gap-2 cursor-default p-12'><span><GiPaperWindmill className='text-[#ec4952] animate-spin'/></span>BuildMyResume</p>
               </div>
               <div className="right p-12 hidden sm:block">
-                  <button className='flex items-center gap-2 cursor-pointer bg-[#ec4952] text-white px-6 py-1.5 rounded-full hover:bg-[#ec4960]'><IoNewspaperSharp />Create Resume Now</button>
+                  <button onClick={() => navigate("/ResumeForm")} className='flex items-center gap-2 cursor-pointer bg-[#ec4952] text-white px-6 py-1.5 rounded-full hover:bg-[#ec4960]'><IoNewspaperSharp />Create Resume Now</button>
               </div>
           </div>
       </>
